c5061ca95ce8f9c2f64af1e29bda40fbe862f111
Jagadeesh Kumar <jagadeesh.1724@gmail.com>
2024-03-21 08:19:14 +0000

    Assignment

---
+++ src/components/Home/styledComponents.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const LogoutButton = styled.button`
+  min-width: 150px;
+  align-self: center;
+  border: none;
+  padding: 10px 25px 10px 25px;
+  border-radius: 10px;
+  background-color: #1e293b;
+  margin-top: 30px;
+  font-family: 'Roboto';
+  font-size: 14px;
+  font-weight: 500;
+  color: #ffffff;
+  cursor: pointer;
+  outline: none;
+`
